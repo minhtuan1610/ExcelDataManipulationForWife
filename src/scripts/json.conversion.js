2bd3fe346cba9f02a8e799186b92e74d7195e4b7
Nguyễn Minh Tuấn <tuannm1610@gmail.com>
2025-02-05 00:29:11 +0700

    Update code

--- src/scripts/json.conversion.js
+++ src/scripts/json.conversion.js
@@ -1,10 +1,18 @@
-export function convertXlsx(e) {
-  const data = new Uint8Array(e.target.result);
-  const workbook = XLSX.read(data, { type: "array" });
+import * as XLSX from "../lib/xlsx.full.min.js";
 
-  const firstSheet = workbook.SheetNames[0];
-  const worksheet = workbook.Sheets[firstSheet];
-  const json = XLSX.utils.sheet_to_json(worksheet);
+export function convertXlsx(file, callbackFunc) {
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    const data = new Uint8Array(e.target.result);
+    const workbook = XLSX.read(data, { type: "array" });
 
-  return json;
+    const firstSheet = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[firstSheet];
+    const json = XLSX.utils.sheet_to_json(worksheet);
+    callbackFunc(json);
+  };
+
+  reader.readAsArrayBuffer(file);
 }
+
+convertXlsx();
