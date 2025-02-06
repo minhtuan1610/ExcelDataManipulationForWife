719a44b58596c5a15589f187507eb7dbaa8d2f76
Nguyễn Minh Tuấn <tuannm1610@gmail.com>
2025-02-06 23:20:48 +0700

    Do not use module, because Cuabeo cannot run server

--- TestCode/test.js
+++ TestCode/test.js
@@ -1,8 +1,17 @@
 import { convertXlsx } from "../src/scripts/json.conversion.js";
 
-document.getElementById("convert").addEventListener("click", function () {
+document.getElementById("convert").addEventListener("click", async function () {
   const fileInput = document.getElementById("upload");
-  convertXlsx(fileInput, function (json) {
-    console.log(json);
+  let abc = "g";
+  console.log(abc);
+
+  abc = await new Promise((resolve, reject) => {
+    convertXlsx(fileInput, function (json) {
+      resolve(json);
+      reject("error");
+      // console.log(json);
+    });
   });
+
+  console.log(abc);
 });
